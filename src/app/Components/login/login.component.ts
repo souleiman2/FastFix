e39ec39a0e62a07f975f5880302bbc8f139cb05d
Souleiman <souleiman_ayoub@hotmail.com>
2020-12-23 18:56:43 -0500

    zz

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  loading = false;
+  returnUrl: string;
 
-  ngOnInit(): void {
+  constructor(
+      private route: ActivatedRoute,
+      private router: Router) { }
+
+  ngOnInit() {
+
+      // get return url from route parameters or default to '/'
+      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  login() {
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component'
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 
 const routes: Routes = [
 
   {
-    path: 'succ',
+    path: '',
     component : HomeComponent
+  },{
+    path : 'login',
+    component : LoginComponent
+  },{
+    path : 'register',
+    component : RegisterComponent
   }
 
 
