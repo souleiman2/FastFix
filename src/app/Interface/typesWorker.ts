9476d7e37ed7af7a67060644156ff7c76bdfab50
souleiman2 <souleiman_ayoub@hotmail.com>
2023-01-22 21:25:58 -0500

    modified the text to make it more professional

--- src/app/Interface/typesWorker.ts
+++ src/app/Interface/typesWorker.ts
@@ -4,80 +4,75 @@ export const PORT_CARDS: Array<PortCard> = [
     {
         // <p>Toiture</p>
         img_src: "assets/toiture.jpg",
-        title: "Toiture",
-        sub_title: "Hello there",
+        title: "Roofing",
+        sub_title: "Update your roof",
     },
     {
         // <p>Peinture</p>
         img_src: "assets/painter.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "House painter",
+        sub_title: "Fresh new paint",
     },
     {
         // <p>Plumber</p>
         img_src: "assets/plumber.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Plumbing",
+        sub_title: "Leaks",
     },
     {
         // <p>Plancher</p>
         img_src: "assets/plancher.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Flooring",
+        sub_title: "Redo your floors",
     },
     {
         // <p>Cuisine</p>
         img_src: "assets/kitchen.jpg",
-        title: "Succ ass ma boi",
-        sub_title: "Succ ass ma boi",
+        title: "Change your kitchen",
+        sub_title: "Kitchen",
     },
     {
         // <p>Salle de bain</p>
         img_src: "assets/bathroom.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Bathroom",
+        sub_title: "Change your bathroom",
     },
-    // {
-    //   // <p>Électrician</p>
-    //   img_src : 'assets/electrician.jpg',
-    //   title : 'Here',
-    //   sub_title : 'Hello there'
-    // },
     {
         // <p>Soudure</p>
         img_src: "assets/weld.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Wielding",
+        sub_title: "Wield",
     },
     {
         // <p>Ciment</p>
         img_src: "assets/cement.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Cement",
+        sub_title: "Reinforcement job",
     },
     {
         // <p>Pavé uni</p>
         img_src: "assets/pave_uni.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Cement flooring",
+        sub_title: "Front house flooring",
+        
     },
     {
         // <p>Contracteur général</p>
         img_src: "assets/contractor.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Contractor",
+        sub_title: "General jobs construction",
     },
     {
         // <p>Homme à tout faire</p>
         img_src: "assets/handyman.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Handyman",
+        sub_title: "Unlicensed",
     },
     {
         // <!-- <p>Jardinage</p> -->
         img_src: "assets/gardening.jpg",
-        title: "Here",
-        sub_title: "Hello there",
+        title: "Gardening",
+        sub_title: "Decoration and botanic",
     },
 ];
 
